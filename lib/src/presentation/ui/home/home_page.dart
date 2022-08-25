52ea3fc2c987c80aaa3b5e1646ecc670db14cfa9
Guilherme Pedro Milani <Guilherme.Milani@proway.treina>
2022-08-25 16:54:53 -0300

    [feat] atualizando projeto

--- lib/src/presentation/ui/home/home_page.dart
+++ lib/src/presentation/ui/home/home_page.dart
@@ -83,21 +83,55 @@ class HomePage extends HookConsumerWidget {
                 const Divider(
                   color: Colors.black,
                 ),
-                Visibility(
-                  replacement: const SizedBox(),
-                  visible: stocksActions.isNotEmpty,
+
+                const Padding(
+                  padding: EdgeInsets.only(left: 15, top: 30),
+                  child: Text(
+                    "Novidades",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23,
+                        color: Colors.white),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
                   child: SizedBox(
-                    height: 900,
+                    height: 200,
                     child: ListView.builder(
-                      itemCount: stocksActions.length,
+                      itemCount: 13,
+                      scrollDirection: Axis.horizontal,
                       itemBuilder: (context, index) {
-                        return Text(
-                          stocksActions[index].id.toString(),
-                        );
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 10),
+                              child: Image.network(
+                          "https://legislacaoemercados.capitalaberto.com.br/wp-content/uploads/2020/09/CVM-quer-aumentar-transpar%C3%AAncia-do-aluguel-de-a%C3%A7%C3%B5es-01.png",
+                          scale: 2,
+                        ),
+                            ));
                       },
                     ),
                   ),
-                ),
+                )
+
+                // Visibility(
+                //   replacement: const SizedBox(),
+                //   visible: stocksActions.isNotEmpty,
+                //   child: SizedBox(
+                //     height: 900,
+                //     child: ListView.builder(
+                //       itemCount: stocksActions.length,
+                //       itemBuilder: (context, index) {
+                //         return Text(
+                //           stocksActions[index].id.toString(),
+                //         );
+                //       },
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
